3ae39c3849807180e89339f41e2b95023e28f640
Calmacil <thomas.lenoel@gmail.com>
2017-05-31 22:49:14 +0200

    Markdown editor
    Enabled SimpleMDE on the calendar, next step: the save method YOUHOU

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -1,10 +1,25 @@
 Calendar = {
+  mde : null,
+  mdeConfig : {
+    spellChecker: false,
+    autoSave: {
+      enabled: true,
+      delay: 1000
+    },
+    forceSync : true
+  },
+  
   /**
    * Inits the calendar functions
    * @returns {undefined}
    */
   init: function() {
+    Calendar.mde = new SimpleMDE(Calendar.mdeConfig);
     
+    
+    if (Calendar.mdeConfig.initialValue != undefined && Calendar.mdeConfig.initialValue) {
+      Calendar.mde.togglePreview();
+    }
   }
 };
 
